6bd0e901246310d1c7f2ead6c94a2d07ab42539d
hpweisberg <hpweisberg@gmail.com>
2022-12-13 22:23:39 -0800

    adjust game variables and cached elements

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 /*-------------------------------- Constants --------------------------------*/
-const snake = {
-  head: 1, 
-  body: [...],
-}
 
 
 /*-------------------------------- Variables --------------------------------*/
+const snake = {
+  head: 1, 
+  body: [],
+}
+let food = 100
+let topWall = 1
+let rightWall = 1
+let bottomWall = 1
+let leftWall = 1
+let score = 0
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-const player = document.querySelector('.player')
-const food = document.querySelector('.food')
+const playerEl = document.querySelector('.player')
+const foodEl = document.querySelector('.food')
 const controllerBtnUp = document.querySelector('.up-btb')
 const controllerBtnRight = document.querySelector('.right-btb')
 const controllerBtnDown = document.querySelector('.down-btb')
 const controllerBtnLeft = document.querySelector('.left-btb')
+const scoreEl = document.querySelector('.score')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -23,3 +30,10 @@ const controllerBtnLeft = document.querySelector('.left-btb')
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function init(){
+  snake = {
+    head: 1,
+    body: 2,
+  }
+}
