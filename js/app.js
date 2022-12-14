c2ef4dab9c6b95e6471bed200c0132481494ab1d
hpweisberg <hpweisberg@gmail.com>
2022-12-13 22:13:08 -0800

    create game board and player. Cash element references for buttons, player, and food

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*-------------------------------- Constants --------------------------------*/
+const snake = {
+  head: 1, 
+  body: [...],
+}
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+const player = document.querySelector('.player')
+const food = document.querySelector('.food')
+const controllerBtnUp = document.querySelector('.up-btb')
+const controllerBtnRight = document.querySelector('.right-btb')
+const controllerBtnDown = document.querySelector('.down-btb')
+const controllerBtnLeft = document.querySelector('.left-btb')
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
