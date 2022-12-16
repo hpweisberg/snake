d23b445bc2340e02171771f0f23c1af00d10734b
hpweisberg <hpweisberg@gmail.com>
2022-12-16 12:44:57 -0800

    atempt snake movement

--- js/app.js
+++ js/app.js
@@ -7,45 +7,35 @@
 let snake = {
   head: 1, 
   body: [],
-  direction: 'right'
+  direction: 'right',
+  
 }
 
-let gameBoard = [ // X-axis - 17 / play area 15
-                    // Y-axis - 14 / play area 12
-  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-]
+
 // playerEl =
 // grid-column-start: 8;
 // grid-row-start: 8;
 
+
+let board
 let food = 100
 let topWall = 2
 let rightWall = 2
 let bottomWall = 2
 let leftWall = 2
 let score = 0
-let win, lose, pause
+let win, lose, pause, loction
 
 let columnCounter = 0
 let rowCounter = 0
+let sqrEl
+// let snakeIdx = sqrEl.findIndex(idx => snake === 'snake')
+
 
 
 /*------------------------ Cached Element References ------------------------*/
 const boardEl = document.querySelector('.board')
-const playerEl = document.querySelector('.player')
+let snakeEl = document.querySelector('.snake')
 const foodEl = document.querySelector('.food')
 const controllerEl = document.querySelector('.controller')
 const controllerBtnUp = document.querySelector('.up-btn')
@@ -63,40 +53,79 @@ const grid = document.getElementsByClassName('sqr')
 //   console.log('test')
 // })
 
-controllerBtnUp.addEventListener('click', function(evt){
-  moveUp()
-  // checkForSnakeBody()
-  // checkForWall()
-  // checkForFood()
-  // updateScoreBoard()
-  // render()
+// controllerBtnUp.addEventListener('click', function(evt){
+//   moveUp()
+//   // checkForSnakeBody()
+//   // checkForWall()
+//   // checkForFood()
+//   // updateScoreBoard()
+//   // render()
+// })
+
+const clickSqr = boardEl.addEventListener('click', evt => {
+  console.log(evt.target.id)
+  console.log(sqrEl.target.id)
 })
 
+controllerBtnRight.addEventListener('click', function(evt){
+  moveRight(evt)
+})
 // playerEl.for(function(evt){
 //   console.log('testyy')
 // })
 
 /*-------------------------------- Functions --------------------------------*/
-init()
-console.log(playerEl.getAttribute('id'))
+// console.log(snakeEl.getAttribute('id'))
 function init(){
-  snake = {
-    head: 1,
-    body: [],
-  }
+  //   let board = [ // X-axis - 17 / play area 15
+  //   // Y-axis - 14 / play area 12
+  // [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  // [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  // ]
+  // createGameBoard()
+  // makeSnake()
   // boardEl = null
+  // snakeEl.setAttribute('id')
   score = 0
   render()
 }
+init()
+
+// let snakeBoardLocation = snakeEl.target.sqrEl
 
 function render(){
   createGameBoard()
   updateGameBoard()
   updateScoreBoard()
 }
+
+// function createGameBoard(){
+//   board.forEach((arr, idx) =>{
+//     let arrIdx = idx
+//     arr.forEach((el, idx) =>{
+//       squareDiv = document.createElement('div')
+//       boardEl.appendChild(squareDiv).setAttribute('id', `row:${arrIdx}`)
+//       squareDiv.setAttribute('class', `column:${idx}`)
+//     })
+//   })
+// }
+
+
 function createGameBoard(){
   for (let i = 0; i < 256; i++){
-    let sqrEl = document.createElement('div')
+    sqrEl = document.createElement('div')
     sqrEl.classList.add('sqr')
     sqrEl.id = (i)
     rowCounter++
@@ -108,34 +137,49 @@ function createGameBoard(){
     }
     // console.log(columnCounter, rowCounter)
     sqrEl.style.gridColumn = rowCounter 
-    sqrEl.style.gridRow =  columnCounter
+    sqrEl.style.gridRow = columnCounter
     boardEl.appendChild(sqrEl)
   }
 }
 
-function handleMovment(){
-  if(setTimeout){
-    console.log('time out, move in same direction')
-  } else{
-    moveUp()
-    moveRight()
-    moveDown()
-    moveLeft()
-  }
-  render()
-}
+// function makeSnake(){
+//   snakeEl = {
+//     head: 1,
+//     body: [],
+//     direction: 'right',
+    
+//   }
 
-function moveUp(){
-  // playerEl.style.gridColumn = ((playerEl.style.gridColumn) - 1)
-  // playerEl.style.grid = playerEl.style.grid[16]
-  // playerEl.getAttribute('id')
-  console.log(playerEl.getAttribute('id'))
-  playerEl.style.gridRowStart--
-  // console.log('I want to move up')
-  // console.log(playerEl.style)
-}
-function moveRight(){
-  // playerEl.
+// }
+
+// function handleMovment(evt){
+//   if(setTimeout){
+//     console.log('time out, move in same direction')
+//   } else{
+//     moveUp()
+//     moveRight()
+//     moveDown()
+//     moveLeft()
+//   }
+//   render()
+// }
+
+// function moveUp(){
+//   // playerEl.style.gridColumn = ((playerEl.style.gridColumn) - 1)
+//   // playerEl.style.grid = playerEl.style.grid[16]
+//   // playerEl.getAttribute('id')
+//   console.log(snakeEl.getAttribute('id'))
+//   playerEl.style.gridRowStart--
+//   // playerEl.setAttribute.gridRow - 16
+//   // console.log('I want to move up')
+//   // console.log(playerEl.style)
+// }
+
+function moveRight(evt){
+  // snakeEl.style.id = boardEl.sqrEl.id++
+  snakeEl.setAttribute.id = sqrEl.id
+  //  = evt.target.id
+  console.log(snakeEl.target.id)
   console.log('I want to move right')
 }
 function moveDown(){
